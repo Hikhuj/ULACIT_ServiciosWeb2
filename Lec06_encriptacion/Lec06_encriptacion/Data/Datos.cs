24dab440dbdeb35b9f7fd8550cbe5c539dc689aa
Roger Jose Ulate Rivera <roger.p.designe357@gmail.com>
2021-06-26 22:26:52 -0600

    Varios cambios

---
+++ Lec06_encriptacion/Lec06_encriptacion/Data/Datos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lec06_encriptacion.Data
+{
+    public class Datos
+    {
+        public static List<Login> listaUsuarios = new List<Login>();
+    }
+}
