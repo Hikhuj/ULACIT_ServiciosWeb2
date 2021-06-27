feedccc95de17adbd40c4a90ca86eefb270e9139
Roger Jose Ulate Rivera <roger.p.designe357@gmail.com>
2021-06-26 22:56:57 -0600

    Laboratorio 6

--- Lec06_encriptacion/Lec06_encriptacion/Data/Datos.cs
+++ Lec06_encriptacion/Lec06_encriptacion/Data/Datos.cs
@@ -8,5 +8,8 @@ namespace Lec06_encriptacion.Data
     public class Datos
     {
         public static List<Login> listaUsuarios = new List<Login>();
+
+        // Laboratorio 6
+        public static List<Libro> listaLibros = new List<Libro>();
     }
 }
