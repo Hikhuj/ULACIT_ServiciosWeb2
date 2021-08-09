660b87aea7776fd86cabcf60cceba5c6d33efbc1
Roger Ulate Rivera <roger.p.designe357@gmail.com>
2021-08-08 21:57:14 -0600

    Clase 12

--- Lec12/Lec12 Estudiante/assets/js/tarjetas.js
+++ Lec12/Lec12 Estudiante/assets/js/tarjetas.js
@@ -15,34 +15,67 @@ $(function(){
 		cardNumberField = $('#card-number-field');
 		owner = $('#owner');
 		cardNumber = $('#cardNumber');
-		cvv = $('#cvv');
+		CVV = $('#cvv');
 		mastercard = $('#mastercard');
 		visa = $('#visa');
 		amex = $('#amex');
 		confirmarButton = $('#confirm-purchase');
 		cargarAnios();
-		carNumber.payform('formatCardNumber');
-		CVV.payform('formatCardCVC');
 		bind();
+		cardNumber.payform('formatCardNumber');
+		CVV.payform('formatCardCVC');
 	}
 
 	const bind = () => {
-		cardNumber.keyup = validPlacements;
+		cardNumber.keyup = validarNumero;
 		confirmarButton.on('click',accionValidar);
 	}
 
-	const validarNumber = () => {
-		console.log('vamos bien');
+	const validarNumero = () => {
+		amex.removeClass('transparent');
+		visa.removeClass('transparent');
+		mastercard.removeClass('transparent');
+
+		if(!$.payform.validateCardNumber(cardNumber.val())){
+			cardNumberField.addClass('has-error');
+		}else{
+			cardNumberField.removeClass('has-error');
+			cardNumberField.addClass('has-success');
+		}
+
+		if($.payform.parseCardType(cardNumber.val()) === 'visa'){
+			mastercard.addclass('transparent');
+			amex.addclass('transparent');
+		}else if($.payform.parseCardType(cardNumber.val()) === 'amex'){
+			mastercard.addclass('transparent');
+			visa.addclass('transparent');
+		}else if($.payform.parseCardType(cardNumber.val()) === 'mastercard'){
+			amex.addclass('transparent');
+			visa.addclass('transparent');
+		}
 	}
 
-	const accionValidar = () => {
-		consoe.log('click');
+	const accionValidar = (e) => {
+		e.preventDefault();
+		console.log('Click');
+		let isCardValid = $.payform.validateCardNumber(cardNumber.val());
+		let isCvvCard = $.payform.validateCardCVC(CVV.val());
+
+		if(owner.val().length < 5){
+			alert("Nombre del dueño incorrecto");
+		}else if(!isCardValid){
+			alert("Numero de tarjeta malo");
+		}else if(!isCvvCard){
+			alert("CVV incorrecto");
+		}else{
+			alert("Todo bien!");
+		}
 	}
 
 	const cargarAnios = () => {
 		let anio = new Date().getFullYear();
 		for (var index = 0; index < 10; index++) {
-			anio+= index;
+			let value = anio + index;
 			yearSeleccionado.append(new Option(value, value.toString().substr(-2)));
 		}
 	}
